a39a510d5361b3b5b16572fbf0d10c32907ce4ae
Gom1031 <atheniald@gmail.com>
2023-03-15 09:47:37 +0900

    update

--- 20230314/5622.py
+++ 20230314/5622.py
@@ -1,5 +1,20 @@
 chr = input()
 list_input = []
+
+time = 0
+
+phone = [['A','B','C'], ['D','E','F'], ['G','H','I'], ['J','K','L'], ['M','N','O'], ['P','Q','R','S'], ['T','U','V'], ['W','X','Y','Z']]
 for i in chr:
     list_input.append(i)
-print(list_input)
+
+def get_index(phone, n):
+    for idx1, i in enumerate(phone):
+        for idx2, j in enumerate(i):
+            if j == n:
+                return idx1
+    return None
+for i in list_input:
+    time += get_index(phone, i)
+
+print(time)
+

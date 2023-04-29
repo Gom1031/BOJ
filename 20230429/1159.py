e0c8ba5bfce221b655340a9f041499ecaa33d3f9
Gom1031 <atheniald@gmail.com>
2023-04-29 22:59:46 +0900

    update

---
+++ 20230429/1159.py
@@ -0,0 +1,32 @@
+n = int(input())
+
+# 딕셔너리 생성
+count = {}
+
+for _ in range(n):
+    name = input()
+    first = name[0]
+    
+    # 딕셔너리에 있으면 key값 +1, 없으면 1로 설정
+    if first in count:
+        count[first] += 1
+    else:
+        count[first] = 1
+
+team = []
+
+print(count)
+
+
+# for루프에서는 dic자료형을 반복할수없음
+# .items 메서드를 사용하여 dic자료형을 반복하도록 만듬
+# 
+for letter, count in count.items():
+    if count >= 5:
+        team.append(letter)
+
+if team:
+    team.sort()
+    print(''.join(team))
+else:
+    print("PREDAJA")

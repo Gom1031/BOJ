64447c28d8aae5b26898b0569b525128314c6223
Gom1031 <atheniald@gmail.com>
2023-05-01 18:53:55 +0900

    update

--- 20230430/27903.py
+++ 20230430/27903.py
@@ -1,3 +1,21 @@
-ascii_codes = [103, 111, 109, 49, 48, 51, 49]
-result = ''.join(chr(code) for code in ascii_codes)
-print(result)
+import io
+import sys
+import contextlib
+
+output_buffer = io.StringIO()
+
+# 표준 출력을 output_buffer로 리디렉션
+with contextlib.redirect_stdout(output_buffer):
+    try:
+        print(2 ** int(input()))
+    except:
+        pass
+
+# 출력 캡처
+output = output_buffer.getvalue()
+
+# 출력 길이를 확인하고 출력 초과가 발생하지 않은 경우에만 표준 출력으로 출력
+if len(output) <= 4300:
+    sys.stdout.write(output)
+else:
+    sys.stdout.write("출력 초과")
---
+++ 2023년4월/20230430/27903.py
@@ -0,0 +1,3 @@
+ascii_codes = [103, 111, 109, 49, 48, 51, 49]
+result = ''.join(chr(code) for code in ascii_codes)
+print(result)
